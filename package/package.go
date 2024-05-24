ef0fb21f00a8490ee278a25615fce5ab7bfd5370
jurgen.kluft <jurgen.kluft@gmail.com>
2024-05-24 09:44:05 +0800

    * package; remove centrypkg

--- package/package.go
+++ package/package.go
@@ -4,7 +4,6 @@ import (
 	cbase "github.com/jurgen-kluft/cbase/package"
 	denv "github.com/jurgen-kluft/ccode/denv"
 	ccore "github.com/jurgen-kluft/ccore/package"
-	centry "github.com/jurgen-kluft/centry/package"
 	cunittest "github.com/jurgen-kluft/cunittest/package"
 )
 
@@ -12,14 +11,12 @@ import (
 func GetPackage() *denv.Package {
 	// Dependencies
 	cunittestpkg := cunittest.GetPackage()
-	centrypkg := centry.GetPackage()
 	ccorepkg := ccore.GetPackage()
 	cbasepkg := cbase.GetPackage()
 
 	// The main (cvmem) package
 	mainpkg := denv.NewPackage("cvmem")
 	mainpkg.AddPackage(cunittestpkg)
-	mainpkg.AddPackage(centrypkg)
 	mainpkg.AddPackage(ccorepkg)
 	mainpkg.AddPackage(cbasepkg)
 
@@ -31,7 +28,6 @@ func GetPackage() *denv.Package {
 	// 'cvmem' unittest project
 	maintest := denv.SetupDefaultCppTestProject("cvmem_test", "github.com\\jurgen-kluft\\cvmem")
 	maintest.Dependencies = append(maintest.Dependencies, cunittestpkg.GetMainLib())
-	maintest.Dependencies = append(maintest.Dependencies, centrypkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, cbasepkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, ccorepkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, mainlib)
