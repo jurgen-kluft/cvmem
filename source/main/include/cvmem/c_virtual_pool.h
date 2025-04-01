1229f7c010623c9e994d03c646c548963c31fcaf
jurgen.kluft <jurgen.kluft@gmail.com>
2025-04-01 09:59:17 +0800

    refactor: update variable names for clarity in pool_t class

--- source/main/include/cvmem/c_virtual_pool.h
+++ source/main/include/cvmem/c_virtual_pool.h
@@ -13,24 +13,28 @@ namespace ncore
     {
         template <typename T> class pool_t : public ncore::pool_t<T>
         {
-            u8* m_baseptr;    // memory base pointer
-            u32 m_item_size;  // the size of an item in bytes
-            u32 m_item_count; // current number of items that are used
-            u32 m_item_cap;   // maximum number of items that can be used
-            u32 m_free_index; // index of the first free item
-            u32 m_free_head;  // index of the first free item in the free list
+            u8* m_baseptr;     // memory base pointer
+            u32 m_item_sizeof; // the size of an item in bytes
+            u32 m_item_count;  // current number of items that are used
+            u32 m_item_cap;    // maximum number of items that can be used
+            u32 m_free_index;  // index of the first free item
+            u32 m_free_head;   // index of the first free item in the free list
 
         public:
             pool_t();
 
-            // e.g: init(sizeof(entity_t), 32768, 16777216);
+            // e.g: setup(32768, 16777216);
             bool setup(u32 initial_item_count, u32 maximum_item_count);
             bool teardown();
 
             inline u32 capacity() const { return m_item_cap; }
             inline u32 size() const { return m_item_count; }
 
-            inline T* ptr_at(u32 index) { return (T*)(m_baseptr + index * m_item_size); }
+            inline T*       ptr() { return (T*)m_baseptr; }
+            inline T const* ptr() const { return (T*)m_baseptr; }
+            inline T*       ptr_at(u32 index) { return (T*)(m_baseptr + index * m_item_sizeof); }
+            inline T const* ptr_at(u32 index) const { return (T const*)(m_baseptr + index * m_item_sizeof); }
+            inline u32      idx_of(T const* item) const { return (u32)((u8*)item - m_baseptr) / m_item_sizeof; }
 
         protected:
             virtual u32   v_allocsize() const final;
