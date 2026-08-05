b57e02029c86436c76e1341bb4f0e020877dc440
jurgen.kluft <jurgen.kluft@gmail.com>
2026-08-05 09:06:58 +0800

    * adjust to changes in ccode

--- package/package.go
+++ package/package.go
@@ -1,9 +1,9 @@
 package cvmem
 
 import (
-	denv "github.com/jurgen-kluft/ccode/denv"
 	ccore "github.com/jurgen-kluft/ccore/package"
 	cunittest "github.com/jurgen-kluft/cunittest/package"
+	denv "github.com/jurgen-kluft/go-ide/denv"
 )
 
 const (
